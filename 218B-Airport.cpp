6814b2496ef6c90a900b450d334f0c17ec97acd6
shtn9902 <86907177+shtn9902@users.noreply.github.com>
2021-07-21 01:19:58 +0530

    Update 218B-Airport.cpp

--- 218B-Airport.cpp
+++ 218B-Airport.cpp
@@ -4,7 +4,7 @@
 		Data structure used: MinHeap and MaxHeap
 		My own unique approach of the question
 	*/
-
+// My Unique Approach //
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -75,6 +75,41 @@ int main(){
 		}
 	}
 	
+	cout<<max_ans<<" "<<min_ans;
+}
+
+// Very short approach using MaxHeap and MinHeap//
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	priority_queue<int> q;
+	priority_queue<int, vector<int>,greater<int> > p;
+	int n,m,x;
+	cin>>n>>m;
+	while(m--){
+		cin>>x;
+		q.push(x);
+		p.push(x);
+	}
+	int max_ans=0,min_ans=0;
+	for(int i=0;i<n;i++){
+		int x = q.top();
+		q.pop();
+		max_ans+=x;
+		x-=1;
+		if(x!=0) q.push(x);
+	}
+	for(int i=0;i<n;i++){
+		int x = p.top();
+		p.pop();
+		min_ans+=x;
+		x-=1;
+		if(x!=0) p.push(x);
+	}
 	cout<<max_ans<<" "<<min_ans;
 }
 
